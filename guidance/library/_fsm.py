8f04da6823fb344837eb141ce83b9b7845bd9768
Hudson Cooper <hncooper96@gmail.com>
2024-04-11 16:09:21 -0700

    Subclass interegular.FSM

--- guidance/library/_fsm.py
+++ guidance/library/_fsm.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from collections.abc import Collection, Mapping
-from typing import Callable
+from functools import cached_property
+from typing import Callable, Dict
 
 import interegular
 
@@ -16,28 +16,40 @@ State = int
 TransitionKey = int
 
 
-class FSM:
-    def __init__(
-        self,
-        transition_map: Mapping[State, Mapping[TransitionKey, State]],
-        initial: State,
-        finals: Collection[State],
-        grammars: Mapping[TransitionKey, GrammarFunction],
-    ):
-        self.transition_map = transition_map
-        self.initial = initial
-        self.finals = finals
-        self.grammars = grammars
+class FSM(interegular.FSM):
+
+    @cached_property
+    def grammars(self) -> Dict[TransitionKey, GrammarFunction]:
+        """
+        Interregular's FSM.alphabet.by_transition maps a TransitionKey to the set of
+        all characters that induce that transition. We convert this set of characters
+        to a GrammarFunction.
+        """
+        alphabet = {
+            char
+            for char in self.alphabet.keys()
+            if char != interegular.fsm.anything_else
+        }
+
+        grammars: Dict[TransitionKey, GrammarFunction] = {}
+        for transition_key, chars in self.alphabet.by_transition.items():
+            if interegular.fsm.anything_else in chars:
+                assert [interegular.fsm.anything_else] == chars
+                grammars[transition_key] = any_char_but(alphabet)
+            else:
+                grammars[transition_key] = nice_char_group(chars)
+
+        return grammars
 
     def to_grammar(self) -> GrammarFunction:
         # Mapping of states to GrammarFunctions corresponding to running FSM from the given state.
         # This lies in closure of `build_func` and inner `func` in order to allow `func` to be
         # zero-arg, enabling `Placeholder`-based (mutual) recursion.
-        funcs: Mapping[State, Callable[[], GrammarFunction]] = {}
+        funcs: Dict[State, Callable[[], GrammarFunction]] = {}
 
         def build_func(state: State) -> Callable[[], GrammarFunction]:
             # Mapping of transition keys to next state. This lies in the closure of `func`.
-            transition: Mapping[TransitionKey, State] = self.transition_map[state]
+            transition: Dict[TransitionKey, State] = self.map[state]
 
             # Zero-arg grammar function (will be wrapped in guidance decorator after definition)
             def func(lm):
@@ -47,7 +59,7 @@ class FSM:
                     option = self.grammars[transition_key]
                     # If next state has no transitions out of it, we know `build_func`
                     # will return the null grammar
-                    if self.transition_map[next_state]:
+                    if self.map[next_state]:
                         # If we've already built the next state's grammar function, get it,
                         # otherwise build it and add to `funcs``
                         next_func = funcs.setdefault(next_state, build_func(next_state))
@@ -73,57 +85,12 @@ class FSM:
         return func()
 
     @classmethod
-    def from_interegular_fsm(cls, fsm: interegular.FSM) -> FSM:
-        # TODO: consider making this the __init__, thinly wrapping interegular FSMs
-
-        # Remove redundant states first
-        fsm = fsm.reduce()
-        alphabet = {
-            char
-            for char in fsm.alphabet.keys()
-            if char != interegular.fsm.anything_else
-        }
-        grammars = {}
-        for transition_key, chars in fsm.alphabet.by_transition.items():
-            if interegular.fsm.anything_else in chars:
-                assert [interegular.fsm.anything_else] == chars
-                grammars[transition_key] = any_char_but(alphabet)
-            else:
-                grammars[transition_key] = nice_char_group(chars)
-
+    def from_pattern(cls, pattern: str) -> FSM:
+        fsm = interegular.parse_pattern(pattern).to_fsm()
         return cls(
-            transition_map=fsm.map,
+            alphabet=fsm.alphabet,
+            states=fsm.states,
             initial=fsm.initial,
             finals=fsm.finals,
-            grammars=grammars,
+            map=fsm.map,
         )
-
-    @classmethod
-    def from_pattern(cls, pattern: str) -> FSM:
-        return cls.from_interegular_fsm(interegular.parse_pattern(pattern).to_fsm())
-
-
-class Regex:
-    # TODO: should be a subclass of FSM?
-
-    def __init__(self, fsm: interegular.FSM):
-        self.fsm = fsm
-
-    @classmethod
-    def from_pattern(cls, pattern: str) -> Regex:
-        fsm = interegular.parse_pattern(pattern).to_fsm()
-        return cls(fsm)
-
-    def intersection(self, other: Regex) -> Regex:
-        # TODO: make FSM a thin wrapper around interegular.FSM and put this method there
-        fsm = self.fsm.intersection(other.fsm)
-        return Regex(fsm)
-
-    def __and__(self, other: Regex) -> Regex:
-        return self.intersection(other)
-
-    def to_fsm(self) -> FSM:
-        return FSM.from_interegular_fsm(self.fsm)
-
-    def to_grammar(self) -> GrammarFunction:
-        return self.to_fsm().to_grammar()
