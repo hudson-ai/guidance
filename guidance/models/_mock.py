127ae807209d1b6d58b0603dcf1464ecd57bfbca
Hudson Cooper <hncooper96@gmail.com>
2024-07-08 16:30:27 -0700

    rewrite mock to actually force a byte string

--- guidance/models/_mock.py
+++ guidance/models/_mock.py
@@ -1,14 +1,10 @@
-from typing import Sequence
-
 import numpy as np
 
 from ._model import Engine, Model, Chat
-from ._remote import RemoteEngine
-from ._tokenizer import Tokenizer
 from ._byte_tokenizer import ByteTokenizer
 
 class MockEngine(Engine):
-    def __init__(self, tokenizer, byte_patterns, compute_log_probs, force):
+    def __init__(self, tokenizer, byte_pattern, compute_log_probs):
         super().__init__(tokenizer, compute_log_probs=compute_log_probs)
 
         self._valid_mask = np.zeros(len(tokenizer.tokens))
@@ -18,70 +14,56 @@ class MockEngine(Engine):
                 self._valid_mask[i] = 1.0
             except:
                 pass
-        self.force = force
         self.called_temperatures = []
 
-        # allow a single byte pattern to be passed
-        if isinstance(byte_patterns, (bytes, str)):
-            byte_patterns = [byte_patterns]
-
         # allow for strings to be passed
-        for i, pattern in enumerate(byte_patterns):
-            if isinstance(pattern, str):
-                byte_patterns[i] = pattern.encode("utf8")
-
-        self.byte_patterns = byte_patterns
+        if isinstance(byte_pattern, str):
+            byte_pattern = byte_pattern.encode("utf8")
+        self.byte_pattern = byte_pattern
 
         # seed the random number generator
         self._rand_generator = np.random.default_rng(seed=42)
 
     def get_logits(self, token_ids, forced_bytes, current_temp):
         """Pretends to compute the logits for the given token state."""
-        # build the byte strings
-        byte_string = b"".join(self.tokenizer.tokens[i] for i in token_ids)
+        return (
+            self._rand_generator.standard_normal(len(self.tokenizer.tokens))
+            * self._valid_mask
+        )
 
-        # if we are forcing the bytes patterns then don't allow other tokens
-        if self.force:
-            logits = np.ones(len(self.tokenizer.tokens)) * -np.inf
-
-        # otherwise we randomly generate valid unicode bytes
-        else:
-            logits = (
-                self._rand_generator.standard_normal(len(self.tokenizer.tokens))
-                * self._valid_mask
-            )
-
-        # if we have a pattern that matches then force the next token
-        bias = 100.0
-        if self.byte_patterns is not None:
-            byte_string
-            for p in self.byte_patterns:
-                if p.startswith(byte_string) and len(p) > len(byte_string):
-                    next_token = self.tokenizer.encode(p[len(byte_string) :])[0]
-                    logits[next_token] += bias
-
-        return logits
-
-    def sample_with_temperature(self, logits: np.ndarray, mask: np.ndarray, temperature: float):
+    def get_next_token(self, prompt_tokens: list[int], token_mask: bytes, temperature: float) -> int:
         self.called_temperatures.append(temperature)
-        return super().sample_with_temperature(logits, mask, 0.)
+        if self.byte_pattern is not None:
+            # If we have a byte pattern, we should use it to determine the next token
+            # (forced even if invalid) otherwise, we will fall back to random valid tokens
+
+            # build the byte string from the tokens
+            bstr = self.tokenizer.decode(prompt_tokens)
+
+            if self.byte_pattern.startswith(bstr) and len(bstr) < len(self.byte_pattern):
+                # TODO: only tokenize head to avoid repeated work
+                next_token = self.tokenizer.encode(self.byte_pattern[len(bstr) :])[0]
+                # Note: may force a bad token -- we will get an exception from the engine
+                return next_token
+
+            # Better MockException?
+            raise Exception("Byte pattern does not match")
+
+        return super().get_next_token(prompt_tokens, token_mask, temperature)
+
 
 class Mock(Model):
     def __init__(
         self,
-        byte_patterns=[],
+        byte_pattern=None,
         echo=True,
         compute_log_probs=False,
-        force=False,
-        **kwargs,
     ):
         """Build a new Mock model object that represents a model in a given state."""
 
-        if isinstance(byte_patterns, str) and byte_patterns.startswith("http"):
-            engine = RemoteEngine(byte_patterns, **kwargs)
-        else:
-            tokenizer = ByteTokenizer()
-            engine = MockEngine(tokenizer, byte_patterns, compute_log_probs, force)
+        # TODO: allow other tokenizers? May be useful for testing
+        tokenizer = ByteTokenizer()
+        engine = MockEngine(tokenizer, byte_pattern, compute_log_probs)
 
         super().__init__(engine, echo=echo)
 
