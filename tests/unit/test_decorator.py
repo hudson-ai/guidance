261867f7207c4ed6b19d9777ea7b638a4b07f8da
Hudson Cooper <hncooper96@gmail.com>
2024-07-09 10:38:19 -0700

    add tests to assert dedent=True works

---
+++ tests/unit/test_decorator.py
@@ -0,0 +1,29 @@
+import guidance
+import re
+
+class TestDedent:
+
+    def test_string(self):
+        @guidance(stateless=True, dedent=True)
+        def string(lm):
+            lm += """\
+            first line
+            second line
+            third line"""
+            return lm
+        lm = guidance.models.Mock()
+        lm += string()
+        assert str(lm) == "first line\nsecond line\nthird line"
+
+    def test_fstring(self):
+        @guidance(stateless=True, dedent=True)
+        def f_string(lm):
+            lm += f"""\
+            {{
+                "name": "{guidance.gen('name', stop='"', max_tokens=1)}",
+            }}
+            """
+            return lm
+        lm = guidance.models.Mock()
+        lm += f_string()
+        assert re.fullmatch(r'\{\n    \"name\": \"[^\"]+\",\n\}\n', str(lm))
