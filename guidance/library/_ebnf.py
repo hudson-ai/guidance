c0db2572e04513b33609a4c914e997059fdd46cd
Hudson Cooper <hncooper96@gmail.com>
2024-08-20 08:21:04 +0100

    black

--- guidance/library/_ebnf.py
+++ guidance/library/_ebnf.py
@@ -18,18 +18,20 @@ class EBNF:
         return cls(parser)
 
     @classmethod
-    def from_package(cls, package: str, grammar_path: str, search_paths: Sequence[str]=[""], **kwargs):
+    def from_package(
+        cls, package: str, grammar_path: str, search_paths: Sequence[str] = [""], **kwargs
+    ):
         parser = Lark.open_from_package(package, grammar_path, search_paths, **kwargs)
         return cls(parser)
-    
+
     @classmethod
     def from_grammar_string(cls, grammar: str, **kwargs):
         parser = Lark(grammar, **kwargs)
         return cls(parser)
-        
+
     def __init__(self, parser: Lark):
         self.parser = parser
-        self.start = self.parser.options.options['start']
+        self.start = self.parser.options.options["start"]
 
         # grammars for nonterminals -- regex seems to be the simplest solution
         self.terminal_regexes: dict[Terminal, str] = {
@@ -50,9 +52,7 @@ class EBNF:
         # Callables to produce grammars for nonterminals
         # They need to be callables rather than literal grammars to avoid
         # infinite recursion (taking advantage of late binding)
-        self.nonterminal_grammar_callables: dict[
-            Terminal, Callable[[], GrammarFunction]
-        ] = {}
+        self.nonterminal_grammar_callables: dict[Terminal, Callable[[], GrammarFunction]] = {}
 
     def build_term(self, term: Union[Terminal, NonTerminal]) -> GrammarFunction:
         if isinstance(term, Terminal):
@@ -62,9 +62,7 @@ class EBNF:
                 term, self.build_nonterminal(term)
             )
             return grammar_callable()
-        raise TypeError(
-            f"term must be one of type Union[Terminal, NonTerminal], got {type(term)}"
-        )
+        raise TypeError(f"term must be one of type Union[Terminal, NonTerminal], got {type(term)}")
 
     def build_rule(self, rule: Rule) -> GrammarFunction:
         terms = [self.build_term(term) for term in rule.expansion]
@@ -74,18 +72,14 @@ class EBNF:
         else:
             return Join(terms, name=rule.alias)
 
-    def build_nonterminal(
-        self, nonterminal: NonTerminal
-    ) -> Callable[[], GrammarFunction]:
+    def build_nonterminal(self, nonterminal: NonTerminal) -> Callable[[], GrammarFunction]:
         # No-arg function to be wrapped in guidance decorator.
         #   - Associated with exactly one nonterminal
         #   - Needs to be no-arg to allow for mutual recursion via `Placeholder`s
         #   - Wrap in guidance decorator later so that we can set the __name__ first
         def inner(lm):
             # Options to select over (one for each rule associated with a nonterminal)
-            options = [
-                self.build_rule(rule) for rule in self.rules_by_nonterminal[nonterminal]
-            ]
+            options = [self.build_rule(rule) for rule in self.rules_by_nonterminal[nonterminal]]
             return lm + select(options)
 
         # Set name and wrap
@@ -94,16 +88,12 @@ class EBNF:
 
     def build(self, name=None) -> GrammarFunction:
         # Trigger recursive build of grammar using start nonterminal
-        body = select([
-            self.build_term(NonTerminal(s))
-            for s in self.start
-        ])
-        ignore_rx = '|'.join(self.terminal_regexes[Terminal(name)] for name in self.parser.ignore_tokens)
-        return subgrammar(
-            name=name,
-            body=body,
-            skip_regex=ignore_rx
+        body = select([self.build_term(NonTerminal(s)) for s in self.start])
+        ignore_rx = "|".join(
+            self.terminal_regexes[Terminal(name)] for name in self.parser.ignore_tokens
         )
+        return subgrammar(name=name, body=body, skip_regex=ignore_rx)
+
 
 def ebnf():
     pass
